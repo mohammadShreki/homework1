c009bc60de4d6326ce6cc8a77dd398d5f38a15b3
mohammadShreki <106436531+mohammadShreki@users.noreply.github.com>
2022-05-31 06:42:02 -0500

    Add files via upload

---
+++ 1-A.py
@@ -0,0 +1,6 @@
+garduated_students=['mohammad','maher','faesal','ali','samer']
+sname=input('enter student name:')
+if sname in garduated_students:
+    print(sname,'is graduated')
+else:
+    print(sname,'is not graduated')
